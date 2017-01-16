f2ccc2d1afb20d2e57b373f2bd32c78cbf9e0fa1
xiaoyuancai <yuanle@hudong.com>
2017-01-16 09:55:08 +0800

    Initial Commit

---
+++ Demo_CameraTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_CameraTest
+//
+//  Created by 互动 on 2017/1/16.
+//  Copyright © 2017年 互动. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo_CameraTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_CameraTest
+//
+//  Created by 互动 on 2017/1/16.
+//  Copyright © 2017年 互动. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
